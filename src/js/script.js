b7b8ec0efcdc9d42cb1e4591f9db21e3c5b8ea50
Luara <luafreire16gmail.com>
2025-04-01 09:10:13 -0300

    declaração de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome = 'fiap';
+console.log(nome)
+
+let idade = 18;
+console.log(idade)
+
+const sobrenome = "Soares";
+console.log(sobrenome)
+//undefined
+let aula;
+console.log(aula)
+//null
+let valor = null;
+console.log(valor)
+//{}--> objeto
+let exemplo1={};
+//[]-->array
+let exemplo2=[];
